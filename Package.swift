535fa770340fc02a956539b67f34a4308accc4b1
theg1239 <thegreatestdropper@gmail.com>
2026-04-16 23:30:25 +0530

    Lower macOS platform to v15

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "z0d1akReports",
     platforms: [
-        .macOS(.v26),
+        .macOS(.v15),
     ],
     products: [
         .executable(
